7c949a5f5ecb6900f268611eafc2f6bdb73a90be
Matt Haynie <mrhaynie@live.com>
2019-12-15 16:55:20 -0800

    Add stringops header.

---
+++ cpp/text/stringops.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <initializer_list>
+#include <string>
+
+namespace mh
+{
+	namespace detail
+	{
+		template<typename CharT> inline static constexpr std::initializer_list<CharT> WHITESPACE_CHARS =
+		{
+			CharT(' '),
+			CharT('\n'),
+			CharT('\r'),
+			CharT('\t'),
+			CharT('\v'),
+			CharT('\f'),
+		};
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	std::basic_string<CharT, Traits, Alloc> trim_start(std::basic_string<CharT, Traits, Alloc>&& str, std::initializer_list<CharT> trimChars)
+	{
+		size_t charsToTrim = 0;
+		for (CharT c : str)
+		{
+			bool found = false;
+			for (CharT trimChar : trimChars)
+			{
+				if (c == trimChar)
+				{
+					found = true;
+					break;
+				}
+			}
+
+			if (!found)
+				break;
+
+			charsToTrim++;
+		}
+
+		str.erase(0, charsToTrim);
+		return std::move(str);
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	inline std::basic_string<CharT, Traits, Alloc> trim_start(std::basic_string<CharT, Traits, Alloc>&& str)
+	{
+		return mh::trim_start(std::move(str), mh::detail::WHITESPACE_CHARS<CharT>);
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	std::basic_string<CharT, Traits, Alloc> trim_end(std::basic_string<CharT, Traits, Alloc>&& str, std::initializer_list<CharT> trimChars)
+	{
+		size_t charsToTrim = 0;
+		for (size_t i = str.size(); i--; )
+		{
+			CharT c = str[i];
+
+			bool found = false;
+			for (CharT trimChar : trimChars)
+			{
+				if (c == trimChar)
+				{
+					found = true;
+					break;
+				}
+			}
+
+			if (!found)
+				break;
+
+			charsToTrim++;
+		}
+
+		str.erase(str.size() - charsToTrim);
+		return std::move(str);
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	inline std::basic_string<CharT, Traits, Alloc> trim_end(std::basic_string<CharT, Traits, Alloc>&& str)
+	{
+		return mh::trim_end(std::move(str), mh::detail::WHITESPACE_CHARS<CharT>);
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	inline std::basic_string<CharT, Traits, Alloc> trim(std::basic_string<CharT, Traits, Alloc>&& str, std::initializer_list<CharT> trimChars)
+	{
+		return mh::trim_start(mh::trim_end(std::move(str), trimChars), trimChars);
+	}
+
+	template<typename CharT = char, typename Traits = std::char_traits<CharT>, typename Alloc = std::allocator<CharT>>
+	inline std::basic_string<CharT, Traits, Alloc> trim(std::basic_string<CharT, Traits, Alloc>&& str)
+	{
+		return mh::trim(std::move(str), mh::detail::WHITESPACE_CHARS<CharT>);
+	}
+}
