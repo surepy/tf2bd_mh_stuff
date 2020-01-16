a9e60cfeafb8caf9a7c81dd41cff3782f453445a
Matt Haynie <mrhaynie@live.com>
2020-01-15 17:29:12 -0800

    Move many bits.hpp helpers into the detail namespace.

--- cpp/include/mh/data/bits.hpp
+++ cpp/include/mh/data/bits.hpp
@@ -31,7 +31,23 @@ namespace mh
 		fast,
 	};
 
-	namespace detail { namespace bits_hpp
+	template<typename T> constexpr std::array<T, std::numeric_limits<std::make_unsigned_t<T>>::digits + 1> BIT_MASKS = []
+	{
+		using unsigned_t = std::make_unsigned_t<T>;
+		constexpr unsigned_t digits = std::numeric_limits<unsigned_t>::digits;
+		std::array<T, digits + 1> retVal{};
+
+		for (unsigned_t i = 1; i <= digits; i++)
+		{
+			constexpr auto maxBits = sizeof(uintmax_t) * std::numeric_limits<unsigned char>::digits;
+			constexpr auto mask = std::numeric_limits<uintmax_t>::max();
+			retVal[i] = mask >> (maxBits - i);
+		}
+
+		return retVal;
+	}();
+
+	namespace detail::bits_hpp
 	{
 		static constexpr unsigned BITS_PER_BYTE = std::numeric_limits<std::underlying_type_t<std::byte>>::digits;
 
@@ -98,23 +114,156 @@ namespace mh
 			//	f();
 #endif
 		}
-	} }
 
-	template<typename T> constexpr std::array<T, std::numeric_limits<std::make_unsigned_t<T>>::digits + 1> BIT_MASKS = []
-	{
-		using unsigned_t = std::make_unsigned_t<T>;
-		constexpr unsigned_t digits = std::numeric_limits<unsigned_t>::digits;
-		std::array<T, digits + 1> retVal{};
-
-		for (unsigned_t i = 1; i <= digits; i++)
+		template<unsigned shift_left, unsigned shift_right, typename T = void>
+		[[nodiscard]] constexpr T shift_both(T value)
 		{
-			constexpr auto maxBits = sizeof(uintmax_t) * std::numeric_limits<unsigned char>::digits;
-			constexpr auto mask = std::numeric_limits<uintmax_t>::max();
-			retVal[i] = mask >> (maxBits - i);
+			if constexpr (shift_left >= shift_right)
+				return T(value << (shift_left - shift_right));
+			else
+				return T(value >> (shift_right - shift_left));
 		}
 
-		return retVal;
-	}();
+
+		template<typename TDst>
+		constexpr void byte_write(TDst* dst, size_t index, std::byte value)
+		{
+			using namespace detail::bits_hpp;
+			using TDstR = std::conditional_t<std::is_same_v<TDst, std::byte>, std::underlying_type_t<std::byte>, TDst>;
+
+			debug([&]{ std::cerr << "byte_write " << std::hex << unsigned(value) << " @ " << index << '\n'; });
+
+#if (__cpp_lib_is_constant_evaluated >= 201811)
+			if (!std::is_constant_evaluated())
+			{
+#if MH_BITS_ENABLE_SMALL_MEMCPY
+				memcpy(reinterpret_cast<std::byte*>(dst) + index, &value, 1);
+				return;
+#endif
+			}
+#endif
+
+			if constexpr (sizeof(TDst) == 1)
+			{
+				dst[index] = TDst(value);
+			}
+			else
+			{
+				const auto dst_index = index / sizeof(TDst);
+				constexpr TDstR mask = BIT_MASKS<TDstR>[BITS_PER_BYTE];
+
+				const auto bit_offset = (index % sizeof(TDst)) * BITS_PER_BYTE;
+				dst[dst_index] &= ~(mask << bit_offset);
+				dst[dst_index] |= TDstR(value) << bit_offset;
+			}
+		}
+
+		template<typename TDst = std::byte, typename TSrc = void>
+		[[nodiscard]] constexpr TDst byte_read(const TSrc* src, size_t index)
+		{
+			using namespace detail::bits_hpp;
+			const auto real_index = index / sizeof(TSrc);
+			const auto offset = (index % sizeof(TSrc)) * BITS_PER_BYTE;
+			auto value = src[real_index];
+			value >>= offset;
+			debug([&]{ std::cerr << "byte_read 0x" << std::hex << intprint(std::byte(value)) << " @ "
+				<< std::dec << index << ", real_index " << real_index << ", offset " << offset << '\n'; });
+			return TDst(value);
+		}
+
+		template<typename TSrc>
+		[[nodiscard]] constexpr uint_fast16_t u16_read(const TSrc* src, size_t byte_index)
+		{
+			using namespace detail::bits_hpp;
+
+			uint_fast16_t retVal{};
+#if (__cpp_lib_is_constant_evaluated >= 201811)
+			if (!std::is_constant_evaluated())
+			{
+#if MH_BITS_ENABLE_UNALIGNED_INTEGERS
+				auto u8 = reinterpret_cast<const uint8_t*>(src);
+				retVal = *reinterpret_cast<const uint16_t*>(&u8[byte_index]);
+				return retVal;
+#elif MH_BITS_ENABLE_SMALL_MEMCPY
+				std::memcpy(&retVal, reinterpret_cast<const std::byte*>(src) + byte_index, 2);
+				return retVal;
+#endif
+			}
+#endif
+
+			if (sizeof(TSrc) == sizeof(uint16_t) && (byte_index % sizeof(TSrc)) == 0)
+			{
+				retVal = uint_fast16_t(src[byte_index / sizeof(TSrc)]);
+			}
+			else
+			{
+				retVal = byte_read<uint_fast16_t>(src, byte_index);
+				retVal |= byte_read<uint_fast16_t>(src, byte_index + 1) << BITS_PER_BYTE;
+			}
+
+			debug([&]{ std::cerr << "u16_read " << std::hex << retVal << " @ " << byte_index << '\n'; });
+			return retVal;
+		}
+
+		template<typename TDst>
+		constexpr void u16_write(TDst* dst, size_t byte_index, uint_fast16_t value)
+		{
+			using namespace detail::bits_hpp;
+
+			debug([&]{ std::cerr << "u16_write " << std::hex << value << " @ " << byte_index << '\n'; });
+
+#if (__cpp_lib_is_constant_evaluated >= 201811)
+			if (!std::is_constant_evaluated())
+			{
+#if MH_BITS_ENABLE_UNALIGNED_INTEGERS
+				auto u8 = reinterpret_cast<uint8_t*>(dst);
+				*reinterpret_cast<uint16_t*>(&u8[byte_index]) = value;
+				return;
+#elif MH_BITS_ENABLE_SMALL_MEMCPY
+				std::memcpy(reinterpret_cast<std::byte*>(dst) + byte_index, &value, 2);
+				return;
+#endif
+			}
+#endif
+
+			if constexpr (sizeof(TDst) == sizeof(uint16_t))
+			{
+				if ((byte_index % sizeof(TDst)) == 0)
+				{
+					dst[byte_index / sizeof(TDst)] = TDst(value);
+					return;
+				}
+			}
+
+			byte_write(dst, byte_index, std::byte(value));
+			byte_write(dst, byte_index + 1, std::byte(value >> BITS_PER_BYTE));
+		}
+
+		template<size_t bit_count, size_t dst_offset = 0, size_t src_offset = 0, typename TDst = void, typename TSrc = void>
+		[[nodiscard]] constexpr TDst bit_merge(TDst existing_bits, TSrc new_bits_source)
+		{
+			using namespace detail::bits_hpp;
+			static_assert((bit_count + src_offset) <= (sizeof(TSrc) * BITS_PER_BYTE));
+			static_assert((bit_count + dst_offset) <= (sizeof(TDst) * BITS_PER_BYTE));
+			using TSrcR = std::conditional_t<std::is_same_v<TSrc, std::byte>, std::underlying_type_t<std::byte>, TSrc>;
+			using TDstR = std::conditional_t<std::is_same_v<TDst, std::byte>, std::underlying_type_t<std::byte>, TDst>;
+
+			using ct = std::make_unsigned_t<std::common_type_t<TSrcR, TDstR>>;
+
+			ct src_value = ct(new_bits_source);
+			ct dst_value = ct(existing_bits);
+			debug([&]{ std::cerr << "bit_merge: src_value = " << src_value << ", " << dst_value << '\n'; });
+
+			src_value >>= src_offset;
+			src_value &= BIT_MASKS<ct>[bit_count];
+			src_value <<= dst_offset;
+
+			dst_value &= ~(BIT_MASKS<ct>[bit_count] << dst_offset);
+			dst_value |= src_value;
+
+			return TDst(dst_value);
+		}
+	}
 
 	template<unsigned bits, int_for_bits_mode mode = int_for_bits_mode::exact> using uint_for_bits_t =
 		decltype(detail::bits_hpp::uint_for_bits_helper<bits, mode>());
@@ -164,86 +313,6 @@ namespace mh
 		return bit_read<TDst, TSrc>(src, bit_count, src_offset, dst_offset);
 	}
 
-	template<size_t bit_count, size_t dst_offset = 0, size_t src_offset = 0, typename TDst = void, typename TSrc = void>
-	[[nodiscard]] constexpr TDst bit_merge(TDst existing_bits, TSrc new_bits_source)
-	{
-		using namespace detail::bits_hpp;
-		static_assert((bit_count + src_offset) <= (sizeof(TSrc) * BITS_PER_BYTE));
-		static_assert((bit_count + dst_offset) <= (sizeof(TDst) * BITS_PER_BYTE));
-		using TSrcR = std::conditional_t<std::is_same_v<TSrc, std::byte>, std::underlying_type_t<std::byte>, TSrc>;
-		using TDstR = std::conditional_t<std::is_same_v<TDst, std::byte>, std::underlying_type_t<std::byte>, TDst>;
-
-		using ct = std::make_unsigned_t<std::common_type_t<TSrcR, TDstR>>;
-
-		ct src_value = ct(new_bits_source);
-		ct dst_value = ct(existing_bits);
-		debug([&]{ std::cerr << "bit_merge: src_value = " << src_value << ", " << dst_value << '\n'; });
-
-		src_value >>= src_offset;
-		src_value &= BIT_MASKS<ct>[bit_count];
-		src_value <<= dst_offset;
-
-		dst_value &= ~(BIT_MASKS<ct>[bit_count] << dst_offset);
-		dst_value |= src_value;
-
-		return TDst(dst_value);
-	}
-
-	template<unsigned shift_left, unsigned shift_right, typename T = void>
-	[[nodiscard]] constexpr T shift_both(T value)
-	{
-		if constexpr (shift_left >= shift_right)
-			return T(value << (shift_left - shift_right));
-		else
-			return T(value >> (shift_right - shift_left));
-	}
-
-	template<typename TDst = std::byte, typename TSrc = void>
-	[[nodiscard]] constexpr TDst byte_read(const TSrc* src, size_t index)
-	{
-		using namespace detail::bits_hpp;
-		const auto real_index = index / sizeof(TSrc);
-		const auto offset = (index % sizeof(TSrc)) * BITS_PER_BYTE;
-		auto value = src[real_index];
-		value >>= offset;
-		debug([&]{ std::cerr << "byte_read 0x" << std::hex << intprint(std::byte(value)) << " @ "
-			<< std::dec << index << ", real_index " << real_index << ", offset " << offset << '\n'; });
-		return TDst(value);
-	}
-
-	template<typename TDst>
-	constexpr void byte_write(TDst* dst, size_t index, std::byte value)
-	{
-		using namespace detail::bits_hpp;
-		using TDstR = std::conditional_t<std::is_same_v<TDst, std::byte>, std::underlying_type_t<std::byte>, TDst>;
-
-		debug([&]{ std::cerr << "byte_write " << std::hex << unsigned(value) << " @ " << index << '\n'; });
-
-#if (__cpp_lib_is_constant_evaluated >= 201811)
-		if (!std::is_constant_evaluated())
-		{
-#if MH_BITS_ENABLE_SMALL_MEMCPY
-			memcpy(reinterpret_cast<std::byte*>(dst) + index, &value, 1);
-			return;
-#endif
-		}
-#endif
-
-		if constexpr (sizeof(TDst) == 1)
-		{
-			dst[index] = TDst(value);
-		}
-		else
-		{
-			const auto dst_index = index / sizeof(TDst);
-			constexpr TDstR mask = BIT_MASKS<TDstR>[BITS_PER_BYTE];
-
-			const auto bit_offset = (index % sizeof(TDst)) * BITS_PER_BYTE;
-			dst[dst_index] &= ~(mask << bit_offset);
-			dst[dst_index] |= TDstR(value) << bit_offset;
-		}
-	}
-
 	template<size_t byte_count, typename TSrc, typename TDst>
 	constexpr void byte_copy(TDst* MH_RESTRICT dst, const TSrc* MH_RESTRICT src)
 	{
@@ -265,112 +334,47 @@ namespace mh
 		}
 	}
 
-	template<typename TSrc>
-	[[nodiscard]] constexpr uint_fast16_t u16_read(const TSrc* src, size_t byte_index)
+	namespace detail::bits_hpp
 	{
-		using namespace detail::bits_hpp;
-
-		uint_fast16_t retVal{};
-#if (__cpp_lib_is_constant_evaluated >= 201811)
-		if (!std::is_constant_evaluated())
+		template<size_t src_offset, size_t dst_offset,
+			bool src_multibyte, bool dst_multibyte, bit_clear_mode clear_mode, uint_fast16_t mask,
+			typename TSrc = void, typename TDst = void>
+		constexpr void bit_copy_helper(TDst* MH_RESTRICT dst, const TSrc* MH_RESTRICT src, size_t index)
 		{
-#if MH_BITS_ENABLE_UNALIGNED_INTEGERS
-			auto u8 = reinterpret_cast<const uint8_t*>(src);
-			retVal = *reinterpret_cast<const uint16_t*>(&u8[byte_index]);
-			return retVal;
-#elif MH_BITS_ENABLE_SMALL_MEMCPY
-			std::memcpy(&retVal, reinterpret_cast<const std::byte*>(src) + byte_index, 2);
-			return retVal;
-#endif
+			using namespace detail::bits_hpp;
+
+			constexpr size_t src_offset_bits = src_offset % BITS_PER_BYTE;
+			constexpr size_t src_offset_bytes = src_offset / BITS_PER_BYTE;
+			constexpr size_t dst_offset_bits = dst_offset % BITS_PER_BYTE;
+			constexpr size_t dst_offset_bytes = dst_offset / BITS_PER_BYTE;
+
+			uint_fast16_t src_buf{};
+			if constexpr (src_multibyte)
+				src_buf = u16_read(src, src_offset_bytes + index);
+			else
+				src_buf = byte_read<uint_fast16_t>(src, src_offset_bytes + index);
+
+			src_buf = shift_both<dst_offset_bits, src_offset_bits>(src_buf);
+			src_buf &= mask;
+
+			uint_fast16_t dst_buf{};
+			if constexpr (dst_multibyte)
+				dst_buf = u16_read(dst, dst_offset_bytes + index);
+			else
+				dst_buf = byte_read<uint_fast16_t>(dst, dst_offset_bytes + index);
+
+			if constexpr (clear_mode == bit_clear_mode::clear_bits)
+				dst_buf &= ~mask;
+
+			dst_buf |= src_buf;
+
+			if constexpr (dst_multibyte)
+				u16_write(dst, dst_offset_bytes + index, dst_buf);
+			else
+				byte_write(dst, dst_offset_bytes + index, std::byte(dst_buf));
+
+			debug([&]{ std::cerr << "bit_copy byte " << index << " (mask " << std::hex << mask << ")\n"; });
 		}
-#endif
-
-		if (sizeof(TSrc) == sizeof(uint16_t) && (byte_index % sizeof(TSrc)) == 0)
-		{
-			retVal = uint_fast16_t(src[byte_index / sizeof(TSrc)]);
-		}
-		else
-		{
-			retVal = byte_read<uint_fast16_t>(src, byte_index);
-			retVal |= byte_read<uint_fast16_t>(src, byte_index + 1) << BITS_PER_BYTE;
-		}
-
-		debug([&]{ std::cerr << "u16_read " << std::hex << retVal << " @ " << byte_index << '\n'; });
-		return retVal;
-	}
-
-	template<typename TDst>
-	constexpr void u16_write(TDst* dst, size_t byte_index, uint_fast16_t value)
-	{
-		using namespace detail::bits_hpp;
-
-		debug([&]{ std::cerr << "u16_write " << std::hex << value << " @ " << byte_index << '\n'; });
-
-#if (__cpp_lib_is_constant_evaluated >= 201811)
-		if (!std::is_constant_evaluated())
-		{
-#if MH_BITS_ENABLE_UNALIGNED_INTEGERS
-			auto u8 = reinterpret_cast<uint8_t*>(dst);
-			*reinterpret_cast<uint16_t*>(&u8[byte_index]) = value;
-			return;
-#elif MH_BITS_ENABLE_SMALL_MEMCPY
-			std::memcpy(reinterpret_cast<std::byte*>(dst) + byte_index, &value, 2);
-			return;
-#endif
-		}
-#endif
-
-		if constexpr (sizeof(TDst) == sizeof(uint16_t))
-		{
-			if ((byte_index % sizeof(TDst)) == 0)
-			{
-				dst[byte_index / sizeof(TDst)] = TDst(value);
-				return;
-			}
-		}
-
-		byte_write(dst, byte_index, std::byte(value));
-		byte_write(dst, byte_index + 1, std::byte(value >> BITS_PER_BYTE));
-	}
-
-	template<size_t src_offset, size_t dst_offset,
-		bool src_multibyte, bool dst_multibyte, bit_clear_mode clear_mode, uint_fast16_t mask,
-		typename TSrc = void, typename TDst = void>
-	constexpr void bit_copy_helper(TDst* MH_RESTRICT dst, const TSrc* MH_RESTRICT src, size_t index)
-	{
-		using namespace detail::bits_hpp;
-
-		constexpr size_t src_offset_bits = src_offset % BITS_PER_BYTE;
-		constexpr size_t src_offset_bytes = src_offset / BITS_PER_BYTE;
-		constexpr size_t dst_offset_bits = dst_offset % BITS_PER_BYTE;
-		constexpr size_t dst_offset_bytes = dst_offset / BITS_PER_BYTE;
-
-		uint_fast16_t src_buf{};
-		if constexpr (src_multibyte)
-			src_buf = u16_read(src, src_offset_bytes + index);
-		else
-			src_buf = byte_read<uint_fast16_t>(src, src_offset_bytes + index);
-
-		src_buf = shift_both<dst_offset_bits, src_offset_bits>(src_buf);
-		src_buf &= mask;
-
-		uint_fast16_t dst_buf{};
-		if constexpr (dst_multibyte)
-			dst_buf = u16_read(dst, dst_offset_bytes + index);
-		else
-			dst_buf = byte_read<uint_fast16_t>(dst, dst_offset_bytes + index);
-
-		if constexpr (clear_mode == bit_clear_mode::clear_bits)
-			dst_buf &= ~mask;
-
-		dst_buf |= src_buf;
-
-		if constexpr (dst_multibyte)
-			u16_write(dst, dst_offset_bytes + index, dst_buf);
-		else
-			byte_write(dst, dst_offset_bytes + index, std::byte(dst_buf));
-
-		debug([&]{ std::cerr << "bit_copy byte " << index << " (mask " << std::hex << mask << ")\n"; });
 	}
 
 	template<size_t bits_to_copy,
