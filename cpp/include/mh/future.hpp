f475665aac03eed28413c37203110cba4008048b
Matt Haynie <mrhaynie@live.com>
2020-07-02 21:08:24 -0700

    should probably make sure the future is valid too

--- cpp/include/mh/future.hpp
+++ cpp/include/mh/future.hpp
@@ -8,6 +8,6 @@ namespace mh
 	template<typename T>
 	inline bool is_future_ready(const T& future)
 	{
-		return future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+		return future.valid() && future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 	}
 }
