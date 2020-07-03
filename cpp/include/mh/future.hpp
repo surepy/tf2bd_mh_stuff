43721a84b63dc3c6dd57c3a96e5b5046196492f8
Matt Haynie <mrhaynie@live.com>
2020-07-02 21:05:01 -0700

    Added mh::is_future_ready because i need it everywhere in tf2_bot_detector

---
+++ cpp/include/mh/future.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <chrono>
+#include <future>
+
+namespace mh
+{
+	template<typename T>
+	inline bool is_future_ready(const T& future)
+	{
+		return future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+	}
+}
