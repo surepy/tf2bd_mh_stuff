f2c388779e950b1f524b60becad357d4be902387
Matt Haynie <mrhaynie@live.com>
2020-09-07 21:33:48 -0700

    * Renamed exception folder to error * Added mh::status_source and mh::status_reader, a thread-safe status reporting object.

---
+++ cpp/include/mh/error/status.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <compare>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <type_traits>
+
+namespace mh
+{
+	namespace detail::status_hpp
+	{
+		template<typename TStatusObj>
+		class status_base
+		{
+		public:
+			using status_obj_type = TStatusObj;
+
+			struct value_type
+			{
+				auto operator<=>(const value_type&) const = default;
+
+				status_obj_type m_Status{};
+				std::string m_Message;
+			};
+
+			bool operator==(const status_base& other) const { return m_SharedData == other.m_SharedData; }
+
+		protected:
+			// Returns true if changed
+			bool set(status_obj_type status, const std::string_view& msg)
+			{
+				std::lock_guard lock(m_SharedData->m_Mutex);
+
+				value_type& value = m_SharedData->m_Value;
+
+				bool changed = false;
+				if (value.m_Status != status)
+				{
+					changed = true;
+					value.m_Status = status;
+				}
+
+				if (value.m_Message != msg)
+				{
+					changed = true;
+					value.m_Message = msg;
+				}
+
+				if (changed)
+					m_SharedData->m_HasValue = true;
+
+				return changed;
+			}
+
+			bool has_value() const
+			{
+				std::lock_guard lock(m_SharedData->m_Mutex);
+				return m_SharedData->m_HasValue;
+			}
+
+			value_type get() const
+			{
+				std::lock_guard lock(m_SharedData->m_Mutex);
+				return m_SharedData->m_Value;
+			}
+
+		private:
+			struct SharedData
+			{
+				mutable std::mutex m_Mutex;
+				value_type m_Value;
+				bool m_HasValue = false;
+			};
+			std::shared_ptr<SharedData> m_SharedData = std::make_shared<SharedData>();
+
+		protected:
+			status_base() = default;
+			explicit status_base(status_obj_type code, std::string msg = {})
+			{
+				set(code, std::move(msg));
+			}
+			status_base(const status_base& other) :
+				m_SharedData(other.m_SharedData)
+			{
+			}
+
+			std::shared_ptr<SharedData> get_shared_data() const { return m_SharedData; }
+		};
+	}
+
+	template<typename TStatusObj>
+	class status_source : public detail::status_hpp::status_base<TStatusObj>
+	{
+	public:
+		using status_base::status_base;
+
+		using status_base::set;
+
+		auto get_reader() const;
+	};
+
+	template<typename TStatusObj>
+	class status_reader : public detail::status_hpp::status_base<TStatusObj>
+	{
+	public:
+		status_reader()
+		{
+		}
+		status_reader(const status_source<TStatusObj>& source) :
+			detail::status_hpp::status_base<TStatusObj>(source)
+		{
+		}
+
+		using status_base::get;
+		using status_base::has_value;
+	};
+
+	template<typename TStatusObj>
+	auto status_source<TStatusObj>::get_reader() const
+	{
+		return status_reader(*this);
+	}
+}
