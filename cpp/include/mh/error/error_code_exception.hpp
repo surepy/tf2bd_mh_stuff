568f3c72e4e3c95fc657c90d92f2769bcc7813eb
Matt Haynie <mrhaynie@live.com>
2020-09-17 16:13:15 -0700

    Improvements to mh::error_code_exception/mh::error_condition_exception.

--- cpp/include/mh/error/error_code_exception.hpp
+++ cpp/include/mh/error/error_code_exception.hpp
@@ -7,42 +7,41 @@
 
 namespace mh
 {
-	namespace detail::error_code_exception
+	template<typename TBase, typename TError = std::error_code>
+	class basic_error_code_exception : public TBase
 	{
-		template<typename TBase, typename TError>
-		class basic_error_code_exception : public TBase
+	public:
+		using base_type = TBase;
+		using code_type = TError;
+
+	private:
+		static std::string format_message(const code_type& code, const std::string_view& message)
 		{
-		public:
-			using base_type = TBase;
-			using code_type = TError;
+			std::string retVal;
+			retVal
+				.append("(")
+				.append(std::to_string(+code.value()))
+				.append(") ")
+				.append(code.message());
 
-		private:
-			static std::string format_message(const code_type& code, const std::string_view& message)
-			{
-				std::string retVal = "(" + std::to_string(+code.value()) + ") " + code.message();
+			if (!message.empty())
+				retVal.append(": ").append(message);
 
-				if (!message.empty())
-					retVal += ": " + message;
+			return retVal;
+		}
 
-				return retVal;
-			}
+	public:
+		basic_error_code_exception(code_type code, const std::string_view& message = {}) :
+			base_type(format_message(code, message)),
+			m_Code(std::move(code))
+		{
+		}
 
-		public:
-			basic_error_code_exception(code_type code, const std::string_view& message = {}) :
-				base_type(format_message(code, message)),
-				m_Code(std::move(code))
-			{
-			}
+		const code_type& code() const { return m_Code; }
 
-			const code_type& code() const { return m_Code; }
-
-		private:
-			code_type m_Code;
-		};
-	}
-
-	template<typename TBase, typename TError = std::error_code> using basic_error_code_exception =
-		detail::error_code_exception::basic_error_code_exception<TBase, std::error_code>;
+	private:
+		code_type m_Code;
+	};
 
 	template<typename TBase> using basic_error_condition_exception =
 		basic_error_code_exception<TBase, std::error_condition>;
