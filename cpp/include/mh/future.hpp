bc51ecad4b604d11c09aab852ee58df238717189
Matt Haynie <mrhaynie@live.com>
2020-12-21 11:29:24 -0800

    Allowed emplace_ready_future to result in a failed future on exception.

--- cpp/include/mh/future.hpp
+++ cpp/include/mh/future.hpp
@@ -12,15 +12,6 @@ namespace mh
 		return future.valid() && future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 	}
 
-	template<typename T, typename... TArgs>
-	auto emplace_ready_future(TArgs&&... args) ->
-		decltype(T(std::forward<TArgs>(args)...), std::future<T>{})
-	{
-		std::promise<T> promise;
-		promise.set_value(T(std::forward<TArgs>(args)...));
-		return promise.get_future();
-	}
-
 	template<typename T>
 	std::future<T> make_ready_future(T&& value)
 	{
@@ -48,5 +39,19 @@ namespace mh
 	std::future<T> make_failed_future(const E& exception)
 	{
 		return make_failed_future<T>(std::make_exception_ptr(exception));
+	}
+
+	template<typename T, typename... TArgs>
+	auto emplace_ready_future(TArgs&&... args) ->
+		decltype(T(std::forward<TArgs>(args)...), std::future<T>{})
+	{
+		try
+		{
+			return make_ready_future(T(std::forward<TArgs>(args)...));
+		}
+		catch (...)
+		{
+			return make_failed_future<T>(std::current_exception());
+		}
 	}
 }
