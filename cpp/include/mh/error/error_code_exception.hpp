154f1073227750a6b9620bbfb04d6db053da1280
Matt Haynie <mrhaynie@live.com>
2021-01-11 13:51:07 -0800

    Changed mh::basic_error_code_exception to be be based off of std::system_error

--- cpp/include/mh/error/error_code_exception.hpp
+++ cpp/include/mh/error/error_code_exception.hpp
@@ -7,44 +7,25 @@
 
 namespace mh
 {
-	template<typename TBase, typename TError = std::error_condition>
-	class basic_error_code_exception : public TBase
+	template<typename TError = std::error_condition>
+	class basic_error_code_exception : public std::system_error
 	{
 	public:
-		using base_type = TBase;
+		using base_type = std::system_error;
 		using code_type = TError;
 
-	private:
-		static std::string format_message(const code_type& code, const std::string_view& message)
-		{
-			std::string retVal;
-			retVal
-				.append("(")
-				.append(std::to_string(+code.value()))
-				.append(") ")
-				.append(code.message());
-
-			if (!message.empty())
-				retVal.append(": ").append(message);
-
-			return retVal;
-		}
-
 	public:
-		basic_error_code_exception(code_type code, const std::string_view& message = {}) :
-			base_type(format_message(code, message)),
-			m_Code(std::move(code))
+		basic_error_code_exception(code_type code, const std::string& message = {}) :
+			base_type(code.value(), code.category(), message)
 		{
 		}
 
-		const code_type& code() const { return m_Code; }
-
-	private:
-		code_type m_Code;
+		code_type code() const
+		{
+			auto baseCode = base_type::code();
+			return code_type(baseCode.value(), baseCode.category());
+		}
 	};
 
-	template<typename TBase> using basic_error_condition_exception =
-		basic_error_code_exception<TBase, std::error_condition>;
-
-	using error_condition_exception = basic_error_condition_exception<std::runtime_error>;
+	using error_condition_exception = basic_error_code_exception<>;
 }
