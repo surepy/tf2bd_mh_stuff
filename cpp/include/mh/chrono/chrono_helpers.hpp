370027c94250a0a296fa0a8d5075cb0db99eb1df
Matt Haynie <mrhaynie@live.com>
2021-01-17 13:22:09 -0800

    Added chrono/chrono_helpers.hpp, which supports converting between a few different formats of time.

---
+++ cpp/include/mh/chrono/chrono_helpers.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <chrono>
+#include <ctime>
+
+namespace mh::chrono
+{
+	template<typename T = double, typename TRep, typename TPeriod>
+	inline constexpr T to_seconds(const std::chrono::duration<TRep, TPeriod>& duration)
+	{
+		return std::chrono::duration_cast<std::chrono::duration<T>>(duration).count();
+	}
+
+	enum class time_zone
+	{
+		local = -100000,
+		utc = 0,
+	};
+
+	std::time_t to_time_t(const std::chrono::system_clock::time_point& t);
+	std::time_t to_time_t(std::tm t, time_zone zone);
+
+	std::tm to_tm(const std::chrono::system_clock::time_point& t, time_zone zone);
+	std::tm to_tm(const std::time_t& t, time_zone zone);
+
+	std::chrono::system_clock::time_point to_time_point(const std::tm& t, time_zone zone);
+	std::chrono::system_clock::time_point to_time_point(const std::time_t& t);
+
+	std::tm current_tm(time_zone zone);
+	std::time_t current_time_t();
+	std::chrono::system_clock::time_point current_time_point();
+}
+
+#ifndef MH_COMPILE_LIBRARY
+#include "chrono_helpers.inl"
+#endif
