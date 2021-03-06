6d0f8a16e6c172ab83fe6b15cfc0cae60d5dfde0
Matt Haynie <mrhaynie@live.com>
2021-03-06 14:19:44 -0800

    fix output name

--- runtest.bat
+++ runtest.bat
@@ -7,12 +7,12 @@ MKDIR out_test
 CD out_test
 
 ECHO Configuring...
-cmake ../ -G Ninja || EXIT /b
+cmake ../ -G Ninja -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF || EXIT /b
 
 ECHO Building...
-cmake --build . || EXIT /b
+cmake --build . --config Debug --target install || EXIT /b
 
-ECHO Running CTest...
-ctest --output-on-failure || EXIT /b
+@REM ECHO Running CTest...
+@REM ctest --output-on-failure || EXIT /b
 
 ENDLOCAL
