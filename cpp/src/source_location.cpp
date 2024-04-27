b691ce05997f0299f954ff9cd827e7984a1c137a
surepy <24486494+surepy@users.noreply.github.com>
2024-04-26 23:56:32 -0700

    continue my shitty patch to make it compile for now at least

--- cpp/src/source_location.cpp
+++ cpp/src/source_location.cpp
@@ -1,6 +1,6 @@
 #include "mh/source_location.hpp"
 
-#if defined(MH_FORMATTER) && MH_FORMATTER != MH_FORMATTER_NONE
+#if defined(MH_FORMATTER) && MH_FORMATTER != MH_FORMATTER_NONE && !__linux__
 template struct mh::formatter<mh::source_location, char>;
 template struct mh::formatter<mh::source_location, wchar_t>;
 #endif
